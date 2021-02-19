2edec369ae6400834feb2b627e1e9fbe1d68ddce
Graham Higgins <gjh@bel-epa.com>
2021-02-19 11:39:33 +0000

    rebranding straggler

--- src/qt/blockexplorer.cpp
+++ src/qt/blockexplorer.cpp
@@ -505,7 +505,7 @@ void BlockExplorer::showEvent(QShowEvent*)
 
         if (!gArgs.GetBoolArg("-txindex", false))
         {
-            QString Warning = tr("Not all transactions will be shown. To view all transactions you need to set txindex=1 in the configuration file (vcore.conf).");
+            QString Warning = tr("Not all transactions will be shown. To view all transactions you need to set txindex=1 in the configuration file (datacoin.conf).");
             QMessageBox::warning(this, "Datacoin Blockchain Explorer", Warning, QMessageBox::Ok);
         }
     }
